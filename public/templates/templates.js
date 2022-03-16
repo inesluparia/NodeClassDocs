c65c1057933cf98d1ae64b9441b20e5514632da3
inesluparia <inesluparia@gmail.com>
2022-03-16 21:48:13 +0100

    refactoring of templates.js

--- public/templates/templates.js
+++ public/templates/templates.js
@@ -10,17 +10,10 @@ const day2Html = fs.readFileSync("./public/pages/day2/day2.html").toString()
 const day3Html = fs.readFileSync("./public/pages/day3/day3.html").toString()
 const day4Html = fs.readFileSync("./public/pages/day4/day4.html").toString()
 
-function makeHomePage() {return top.replace("%%TITLE%%", "Node Docs Home") + nav + homeHtml + footer }
-function makeDay1Page() {return top.replace("%%TITLE%%", "Day 1") + nav + day1Html + footer}
-function makeDay2Page() {return top.replace("%%TITLE%%", "Day 2") + nav + day2Html + footer}
-function makeDay3Page() {return top.replace("%%TITLE%%", "Day 3") + nav + day3Html + footer}
-function makeDay4Page() {return top.replace("%%TITLE%%", "Day 4") + nav + day4Html + footer}
-
 module.exports = {
-    //home: () => { return top.replace("%%TITLE%%", "Node Docs Home") + nav + homeHtml + footer}
-    home: makeHomePage(),
-    day1: makeDay1Page(),
-    day2: makeDay2Page(),
-    day3: makeDay3Page(),
-    day4: makeDay4Page()
+    home: top.replace("%%TITLE%%", "Node Docs Home") + nav + homeHtml + footer,
+    day1: top.replace("%%TITLE%%", "Day 1") + nav + day1Html + footer,
+    day2: top.replace("%%TITLE%%", "Day 2") + nav + day2Html + footer,
+    day3: top.replace("%%TITLE%%", "Day 3") + nav + day3Html + footer,
+    day4: top.replace("%%TITLE%%", "Day 4") + nav + day4Html + footer
 }
